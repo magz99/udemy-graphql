100c79179e848051818aee79defa15b3ba74464c
Magali Bautista-Lee <magzb.99@gmail.com>
2021-03-13 10:09:20 -0500

    Adds updateUser

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -48,6 +48,35 @@ const Mutation = {
 
         return deletedUsers[0];
     },
+    updateUser(parent, args, { db }, info) {
+        const { id, data } = args;
+
+        const user = db.users.find((user) => { return user.id === args.id });
+
+        if (!user) {
+            throw new Error('User not found.');
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some((user) => { user.email === data.email });
+
+            if (emailTaken) {
+                throw new Error('Email in use.');
+            }
+
+            user.email = data.email;
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name;
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age;
+        }
+
+        return user;
+    },
     createPost(parent, args, { db }, info) {
         const userExists = db.users.some((user) => {
             return user.id === args.data.author;
